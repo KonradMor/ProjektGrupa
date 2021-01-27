82b4f6fae6663632c57585042cfdb8fe0860c31f
Radzik <kmmoraw@gmail.com>
2021-01-27 18:17:59 +0100

    added features main

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
-from .views import View_to_change
+from .views import (TeamMembersListView, TaskListView, TaskUpdateView,
+                    TaskCreateView, TaskDeleteView, ManagerChangeView,
+                    TeamCreateView, TeamMemberCreateView, TeamMemberDeleteView,
+                    TeamDeleteView, MemberTasksListView
+                    )
+
 
 urlpatterns = [
-    path('', View_to_change)
+    path('', TeamMembersListView.as_view(), name='team_members'),
+    path('team_add', TeamCreateView.as_view(), name='team_add'),
+    path('team_delete/', TeamDeleteView.as_view(), name='team_delete'),
+    path('tasks/', TaskListView.as_view(), name='tasks_list'),
+    path('task_end/<int:pk>', TaskUpdateView.as_view(), name='task_end'),
+    path('task_add/', TaskCreateView.as_view(), name='task_add'),
+    path('task_delete/<int:pk>', TaskDeleteView.as_view(), name='task_delete'),
+    path('manager_change/', ManagerChangeView.as_view(), name='manager_change'),
+    path('member_add/', TeamMemberCreateView.as_view(), name='member_add'),
+    path('member_delete/<int:pk>', TeamMemberDeleteView.as_view(), name='member_delete'),
+    path('member_tasks/<int:pk>', MemberTasksListView.as_view(), name='member_tasks')
 ]
