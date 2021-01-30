d08a266a694611d90aa62959f1c2ffe0be55c7e8
Korneliusz-Rzyman <kornel.rzyman@gmail.com>
2021-01-30 13:08:21 +0100

    messages v2

--- message/views.py
+++ message/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import Messages
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class MessageListView(ListView):
+class MessageListView(LoginRequiredMixin, ListView):
     model = Messages
     template_name = "message.html"
 
-class MessageCreateView(CreateView):
+class MessageCreateView(LoginRequiredMixin, CreateView):
     model = Messages
     template_name = "form.html"
     fields = "__all__"
