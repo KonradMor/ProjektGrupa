cedb95c902bb8a1c6d424646d69c53fef5acdcbf
Radzik <kmmoraw@gmail.com>
2021-01-30 13:03:28 +0100

    added more features

--- message/urls.py
+++ message/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import MessageListView, MessageCreateView
+from .views import MessageListView, MessageCreateView, ContentCreateView
+
+
 urlpatterns = [
     path('', MessageListView.as_view(), name="MessListView"),
-    path('create/', MessageCreateView.as_view(), name="MessCreateView")
+    path('create/', MessageCreateView.as_view(), name="MessCreateView"),
+    path('create_to/<int:pk>', ContentCreateView.as_view(), name='message_to')
 ]
--- message/views.py
+++ message/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import Messages
+from accounts.models import Users
+from main.models import TeamMembers
 from django.urls import reverse_lazy
 # Create your views here.
+
 
 class MessageListView(ListView):
     model = Messages
     template_name = "message.html"
 
+    def get_queryset(self):
+        self.extra_context = {'to': Messages.objects.filter(id_user_to=self.request.user.users)}
+        return Messages.objects.filter(id_user_from=self.request.user.users)
+
+
 class MessageCreateView(CreateView):
     model = Messages
     template_name = "form.html"
-    fields = "__all__"
+    fields = ['content', 'id_user_to']
     success_url = reverse_lazy("MessListView")
+
+    def form_valid(self, form):
+        form.instance.id_user_from = self.request.user.users
+        return super(MessageCreateView, self).form_valid(form)
+
+
+class ContentCreateView(CreateView):
+    model = Messages
+    template_name = "form.html"
+    fields = ['content']
+    success_url = reverse_lazy("MessListView")
+
+    def form_valid(self, form):
+        form.instance.id_user_from = self.request.user.users
+        user_pk = TeamMembers.objects.get(pk=self.request.resolver_match.kwargs['pk']).member_name
+        form.instance.id_user_to = Users.objects.get(pk=user_pk.id)
+        return super(ContentCreateView, self).form_valid(form)
